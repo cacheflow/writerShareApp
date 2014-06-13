c2521d190286ca218ef164e40b2a160c0c78f793
James Milani <james.milani@gmail.com>
2014-06-13 11:12:36 -0700

    first commit and update gemfile

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WriterShareApp::Application.config.secret_key_base = '870e7c1e1d5c6a09627ab6c6fcb4e888691cd11919ee6d1d59b52242c96f987417e5fad9a0c407d5f4d2d845205f45d7ec4385a75fabb7b974041d90bfd92c46'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WriterShareApp::Application.config.session_store :cookie_store, key: '_writerShareApp_session'
