418729020861a50a1a65f240151679a137231215
James Milani <james.milani@gmail.com>
2014-06-13 11:19:15 -0700

    creates the database.yml from figaro. makes the secret_key an ENV variable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-WriterShareApp::Application.config.secret_key_base = '870e7c1e1d5c6a09627ab6c6fcb4e888691cd11919ee6d1d59b52242c96f987417e5fad9a0c407d5f4d2d845205f45d7ec4385a75fabb7b974041d90bfd92c46'
+WriterShareApp::Application.config.secret_key_base = ENV['SECRET_KEY']
