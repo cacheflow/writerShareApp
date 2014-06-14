883efb18201d15e538e9a02a04d75b0d456cfe35
James Milani <james.milani@gmail.com>
2014-06-14 12:41:21 -0700

    fixes rspec... testing environment is working

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,52 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20140614004413) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pieces", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "version_id"
+    t.boolean  "public"
+    t.boolean  "share_with_all_friends"
+    t.string   "share_with_select_friends", default: [], array: true
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "pieces", ["user_id"], name: "index_pieces_on_user_id", using: :btree
+  add_index "pieces", ["version_id"], name: "index_pieces_on_version_id", using: :btree
+
+  create_table "user_friendships", force: true do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "user_friendships", ["user_id", "friend_id"], name: "index_user_friendships_on_user_id_and_friend_id", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.integer  "user_friendship_id"
+    t.integer  "piece_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["piece_id"], name: "index_users_on_piece_id", using: :btree
+  add_index "users", ["user_friendship_id"], name: "index_users_on_user_friendship_id", using: :btree
+
+  create_table "versions", force: true do |t|
+    t.integer  "piece_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "versions", ["piece_id"], name: "index_versions_on_piece_id", using: :btree
 
 end
