be90196546460dd9d069cbc4518a8c5ad075aea3
James Milani <james.milani@gmail.com>
2014-06-14 15:24:24 -0700

    addes a controller test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,29 @@
 require 'spec_helper'
 
 describe UsersController do
+    let :valid_attributes do
+        {
+            name: "James Milani",
+            email: "james@james.com",
+            password: "ilovecheese",
+            password_confirmation: "ilovecheese",
+
+        }
+    end
+
+    describe "GET index" do
+        let! :user_1 do
+            User.create! valid_attributes
+        end
+
+        it "should render the index page" do
+            expect(response).to render_template :index
+        end
+
+        it " " do
+
+        end
+
+    end
 
  end
