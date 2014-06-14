2d30efb6f0b611182d3ea064641ea81d1727bc45
Lex Alexander <l.alexander10@gmail.com>
2014-06-14 16:19:26 -0700

    Committing before I mess something up.

--- app/controllers/versions_controller.rb
+++ app/controllers/versions_controller.rb
@@ -1,22 +1,39 @@
 class VersionsController < ApplicationController
   def index
+    @versions = Versions.all 
   end
 
   def new
+    @version = Version.new
   end
 
   def create
+    @version = Version.create(version_params)
   end
 
   def show
+    @version = Version.find(params[:id])
   end
 
   def edit
+    @version = Version.find(params[:id])
   end
 
   def update
+    @version = Version.update(params:[id])
   end
 
   def destroy
+    @version = Version.destroy(params:[id])
   end
+
+
+
+def version_params 
+  params.required(:version).permit()
+
+
 end
+
+
+
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,9 +21,8 @@ describe User do
 
     it "should be invalid without a password confirmation" do 
         @user.password_confirmation = nil 
-        expect(@password_confirmation).to be_invalid
+        expect(@user).to be_invalid
     end 
-
 
 
     it "should be invalid without a password" do
@@ -32,26 +31,25 @@ describe User do
     end
 
     it "should have an email of the proper format" do
-        @user2 = User.new(name: "home dawg", email: "dogdogdog.com", password: "yuckie")
+        @user2 = User.new(name: "home dawg", email: "dogdogdog.com", password: "yuckie", password_confirmation: "yuckie")
         @user2.save
         expect(@user2).to_not be_persisted
     end
 
     it "should have a unique email" do
-        @user3 = User.new(name: "serious mike", email: "dawgie@snoop.com", password: "quesoplease")
+        @user3 = User.new(name: "serious mike", email: "dawgie@snoop.com", password: "quesoplease", password_confirmation: "quesoplease")
         @user3.save
         expect(@user3).to be_invalid
     end
 
     it "should have a unique name" do
-        @user4 = User.new(name:"home dawg", email: "topdawg@bigdawg.com", password: "kingofdawgs")
+        @user4 = User.new(name:"home dawg", email: "topdawg@bigdawg.com", password: "kingofdawgs", password_confirmation: "kingofdawgs")
         @user4.save
         expect(@user4).to be_invalid
     end
 
     it "should have password equal password_confirmation" do
-        @user.password_confirmation = nil
-        expect(@password).to eql(@password_confirmation)
+        expect(@user.password).to eql(@user.password_confirmation)
     end 
 
 end
