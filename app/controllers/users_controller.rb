aafcd5099afd1f937391ce3a26b0b38207634521
James Milani <james.milani@gmail.com>
2014-06-17 17:23:51 -0700

    fixes some tests and adds some views for sign in and sign out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  
+  before_action :set_user, :only =>[:show, :edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -20,24 +20,22 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+
   end
 
   def edit
-    @user = User.find(params[:id])
+
   end
 
   def update
-    @user = User.find(params[:id])
-    if @user.save 
-      redirect_to user_path(@user)
+    if @user.update(user_params)
+      redirect_to users_path
     else
       render :edit
     end
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     redirect_to new_user_path
   end
