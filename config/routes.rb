4fcee7c40ed24c21be0b2ede370dc23298e036a4
James Milani <james.milani@gmail.com>
2014-06-25 15:47:46 -0700

    adds TinyMCE back in. Gets rid of Mercury. works a bit on the new and index views for versions.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,19 @@
 WriterShareApp::Application.routes.draw do
   
-  mount Mercury::Engine => '/'
-  Mercury::Engine.routes do
+  # put "users/:user_id/versions/:id/edit" => "versions#mercury_update"
 
-  end
+  # mount Mercury::Engine => '/'
+  # Mercury::Engine.routes do
+
+  # end
 
 
   root "sessions#new"
 
   resources :users do
-      resources :versions
+      resources :versions do
+        member { post :mercury_update }
+      end
   end
 
   resources :sessions, :only => [:new, :create, :destroy]
@@ -17,5 +21,9 @@ WriterShareApp::Application.routes.draw do
   resources :user_friendships, :only => [:create, :destroy]
 
   get "users/search" => "users#search"
+
+  # post "users/:id/versions/new" => "versions#index"
+
+
 
 end
--- spec/controllers/user_friendships_controller_spec.rb
+++ spec/controllers/user_friendships_controller_spec.rb
@@ -3,13 +3,13 @@ require 'spec_helper'
 describe UserFriendshipsController, :type => :controller do
 
     describe "POST create" do
-        describe "successful create" do
-            before do
+        before do
                 @user_1 = User.create!(name: "Maggie Smith", email: "maggz@yahoo.com", password: "smithmagick", password_confirmation: "smithmagick")
                 @user_2 = User.create!(name: "Lauren Sanford", email: "laurnie@gmail.com", password: "yippeeday", password_confirmation: "yippeeday")
                 controller.stub(:current_user) { @user_1 }
             end
 
+        describe "successful create" do
             subject {@user_1 }
             it { should be_valid }
 
@@ -26,6 +26,24 @@ describe UserFriendshipsController, :type => :controller do
                 post :create, friend_id: @user_2
                 expect(response).to redirect_to user_path(@user_1)
            end
+        end
+
+        describe "on successful delete" do
+            before do
+                post :create, friend_id: @user_2
+            end
+
+            it "@user_1 should be able destroy a friendship between @user_1 and @user_2" do
+                expect do
+                    delete :destroy, id: @user_1.user_friendships.first.id
+                end.to change(UserFriendship, :count).by(-1)
+            end
+
+            it "should redirect the user to their user page" do
+                delete :destroy, id: @user_1.user_friendships.first.id
+                expect(response).to redirect_to user_path(@user_1)
+            end
+
         end
     end
 end
