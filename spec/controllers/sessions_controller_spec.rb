7de656d9387cbf0c517f9d90a5841215e5b0ea59
James Milani <james.milani@gmail.com>
2014-06-24 17:48:40 -0700

    more tests and passing tests, yay

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -3,20 +3,51 @@ require 'spec_helper'
 describe SessionsController, type: :controller do
     
     describe "POST create" do
+        describe "on successful signin"do
+            before do
+                @joe = User.create!(name: "Joe", email: "joe@joe.com", password: "thisishard", password_confirmation: "thisishard")
+                post :create, session: { :email => @joe.email, :password => @joe.password }
+            end
 
-        before do
-            @joe = User.create!(name: "Joe", email: "joe@joe.com", password: "thisishard", password_confirmation: "thisishard")
-            post :create, session: :remember_token
+            it "should return that joe is valid" do
+                expect(@joe).to be_valid
+            end
+
+            it "should return the user's email" do
+                expect(assigns[:user]).to eq(@joe)
+            end
+
+            it "should authenticate the user with a proper email and password combination" do
+                expect(session[:remember_token]).to eq(@joe.id)
+            end
+
+            it "should set the current user to be user that is logging in" do
+                expect(assigns[:current_user]).to eq(@joe)
+            end
+
+            it "should redirect to the users' index page" do
+                expect(response).to redirect_to users_path
+            end
         end
 
+        describe "on unsuccessful signin" do
+            before do
+                @joe = User.create!(name: "Joe", email: "joe@joe.com", password: "thisishard", password_confirmation: "thisishard")
+                @matt = User.create!(name: "Matt", email: "matt@matt.com", password: "easyeasy", password_confirmation: "easyeasy")
+                post :create, session: { :email => @joe.email, :password => @matt.password }
+            end
+            
+            it "should return joe as the user by his email" do
+                expect(assigns[:user]).to eq(@joe)
+            end
 
-        it "should return the user object" do
-            expect("joe@joe.com").to eq(@joe)
+            it "should not authenticate joe as the user because he is using the wrong password" do
+                expect(session[:remember_token]).to eq(nil)
+            end
+
+            it "should render the new sessions page" do
+                expect(response).to render_template :new
+            end
         end
-
-
-            it { should redirect_to(users_path)}
-
-
     end
 end
--- spec/controllers/user_friendships_controller_spec.rb
+++ spec/controllers/user_friendships_controller_spec.rb
@@ -2,12 +2,12 @@ require 'spec_helper'
 
 describe UserFriendshipsController, :type => :controller do
 
-  describe "POST create" do
+    describe "POST create" do
         describe "successful create" do
             before do
                 @user_1 = User.create!(name: "Maggie Smith", email: "maggz@yahoo.com", password: "smithmagick", password_confirmation: "smithmagick")
                 @user_2 = User.create!(name: "Lauren Sanford", email: "laurnie@gmail.com", password: "yippeeday", password_confirmation: "yippeeday")
-                @current_user = @user_1
+                controller.stub(:current_user) { @user_1 }
             end
 
             subject {@user_1 }
@@ -18,17 +18,14 @@ describe UserFriendshipsController, :type => :controller do
         
             it "should build a friendship between @user_1 and @user_2" do
                 expect do
-                    post :create, id: 
+                    post :create, friend_id: @user_2
                 end.to change(UserFriendship, :count).by(1)
             end
 
-            it { should redirect_to(user_path(current_user)) }
-        end
-
-        describe "with unsuccessful create" do
-
+           it "should redirect the user to their user page" do
+                post :create, friend_id: @user_2
+                expect(response).to redirect_to user_path(@user_1)
+           end
         end
     end
-
-
 end
